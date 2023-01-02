56a82f5b666000888c1facae114221eeef5a50f4
Dalil CHABLIS <dalil01@pop-os.localdomain>
2023-01-02 23:47:13 +0100

    save changes

--- src/core/types/config.type.ts
+++ src/core/types/config.type.ts
@@ -1,6 +1,10 @@
+import { Languages } from "../constants/languages";
+
 export type ConfigType = {
 	translationKeysFile: string; // Translation keys (enums, object) -> Ex : { KEY_1, key2 } or { key_1 = "value", key_2 = "value" }.
 	outputDir?: string;
+	sourceLanguage: Languages;
+	targetLanguages: Languages[] | string[];
 	
 	
 	
