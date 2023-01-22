f408ceebafd9d53f8daf347efd0633d77f4987f9
Dalil CHABLIS <dalil.chablis@gmail.com>
2023-01-22 20:28:35 +0100

    tests: Manual

---
+++ tests/manual/outputs/translated.ts
@@ -0,0 +1,23 @@
+export const translated = {
+	key1: {
+		fr: "Bonjour !",
+		en: "Good morning !",
+		es: "Buenos dias !",
+		nl: "Hallo !",
+		zhCN: "早上好 ！"
+	},
+	key2: {
+		fr: "Connexion",
+		en: "Login",
+		es: "Acceso",
+		nl: "Verbinding",
+		zhCN: "登录"
+	},
+	key3: {
+		fr: "Mon compte",
+		en: "My account",
+		es: "Mi cuenta",
+		nl: "mijn account",
+		zhCN: "我的账户"
+	}
+}
